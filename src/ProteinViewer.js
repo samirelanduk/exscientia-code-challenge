0ffd9c16060f700a6ae05fa292558f5bf1dcb8a0
Sam Ireland <sam.ireland.uk@gmail.com>
2020-09-13 14:19:25 +0100

    Display ligand

--- src/ProteinViewer.js
+++ src/ProteinViewer.js
@@ -7,7 +7,7 @@ const ProteinViewer = props => {
    * The 3D display.
    */
 
-  const { pdb } = props;
+  const { pdb, ligands } = props;
 
   useEffect(() => {
     // Create NGL stage
@@ -16,8 +16,17 @@ const ProteinViewer = props => {
     // Turn PDB string into file object
     const stringBlob = new Blob([pdb], {type: "application/octet-stream"});
 
+    // Turn ligand PDBs into file objects
+    const ligandBlobs = ligands.map(
+      ligand => new Blob([ligand], {type: "application/octet-stream"})
+    );
+
     stage.loadFile(stringBlob, {ext: "pdb"}).then((component) => {
       stage.rep = component.addRepresentation("cartoon");
+      component.autoView();
+    });
+    stage.loadFile(ligandBlobs[0], {ext: "pdb"}).then((component) => {
+      stage.rep = component.addRepresentation("ball+stick");
       component.autoView();
     });
   })
@@ -30,7 +39,8 @@ const ProteinViewer = props => {
 };
 
 ProteinViewer.propTypes = {
-  pdb: PropTypes.string.isRequired
+  pdb: PropTypes.string.isRequired,
+  ligands: PropTypes.array.isRequired,
 };
 
 export default ProteinViewer;
